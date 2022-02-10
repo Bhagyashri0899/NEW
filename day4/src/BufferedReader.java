a1646d1bc0e537b55eb88ad0bca61ef5edf41fb9
bhagyashri <yuvabhagyashripatil@gmail.com>
2022-02-11 01:05:16 +0530

    commit

---
+++ day4/src/BufferedReader.java
@@ -0,0 +1,23 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+class Main {
+  public static void main(String[] args) {
+    char[] array = new char[100];
+
+    try {
+      
+      FileReader file = new FileReader("input.txt");
+      BufferedReader input = new BufferedReader(file);
+      input.read(array);
+      System.out.println("Data in the file: ");
+      System.out.println(array);
+
+      input.close();
+    }
+
+    catch(Exception e) {
+      e.getStackTrace();
+    }
+  }
+}
