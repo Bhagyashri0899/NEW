239b8d2e6c138ce6ed36d4a47163f3697df9eb4a
bhagyashri <yuvabhagyashripatil@gmail.com>
2022-02-21 15:29:28 +0530

    programs on exception handling

---
+++ Exception handling/src/FindFileDemo.java
@@ -0,0 +1,9 @@
+
+class FindFileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Exception handling/src/Main.java
@@ -0,0 +1,11 @@
+
+ class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(5/0);
+        System.out.println("End of program!");
+
+	}
+
+}
