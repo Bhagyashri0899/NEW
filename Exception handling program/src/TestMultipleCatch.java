be1316f7a85101447b9a1441bd284f6ead0d3b46
bhagyashri <yuvabhagyashripatil@gmail.com>
2022-02-22 18:42:55 +0530

    program using try catch finally

---
+++ Exception handling program/src/TestMultipleCatch.java
@@ -0,0 +1,27 @@
+
+public class TestMultipleCatch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try
+		{
+		int a[]=new int[5];
+		a[5]=30/0;
+		}
+		catch(ArithmeticException e)
+		{
+		System.out.println("task1 is completed");
+		}
+		
+	
+     catch(ArrayIndexOutOfBoundsException e)
+     {
+     System.out.println("task 2 completed");
+     }
+     catch(Exception e)
+     {
+     System.out.println("common task completed");
+     }
+     System.out.println("rest of the code...");
+  }
+}
