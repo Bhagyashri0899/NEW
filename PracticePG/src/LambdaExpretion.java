0a2bbcd9dafb6deb59f57cacf256c1a062397d90
bhagyashri <yuvabhagyashripatil@gmail.com>
2022-03-06 22:23:08 +0530

    practice on HACKERRANK

---
+++ PracticePG/src/LambdaExpretion.java
@@ -0,0 +1,105 @@
+//PerformOperation isOdd():The lambda expression must return true if a number is odd or false  if it is even.
+//PerformOperation isPrime():
+//PerformOperation isPalindrome():
+
+import java.io.*;
+import java.util.*;
+interface PerformOperation {
+ boolean check(int a);
+}
+class MyMath {
+ public static boolean checker(PerformOperation p, int num) {
+  return p.check(num);
+ }
+public static PerformOperation isOdd()
+{
+    return num ->
+    {
+          if(num%2==0)
+            return false;
+         else
+            return true;
+    } ;
+}
+
+public static PerformOperation isPrime()
+{
+    return num ->
+    {
+        int flag = 0;
+        for(int i=2;i<=num/2;i++)
+      {
+           if(num%i==0)
+            {
+              flag = 1;
+              break; 
+            }
+           else
+            {
+              flag = 0;
+              break; 
+            }
+            
+      }
+      if(flag == 0)
+          return true;
+      else 
+          return false;
+    }; 
+}
+
+public static PerformOperation isPalindrome()
+{
+    return num ->
+    {
+      int r,sum=0,temp; 
+      temp = num;
+       while(num>0)
+        {
+            r = num % 10;
+            sum = (sum*10)+r;  
+            num = num/10;  
+        }
+      if(temp == sum)
+        return true;
+      else
+        return false;
+    };
+   
+}
+}
+
+
+public class LambdaExpretion {
+	public static void main(String[] args) throws IOException {
+		  MyMath ob = new MyMath();
+		  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		  int T = Integer.parseInt(br.readLine());
+		  PerformOperation op;
+		  boolean ret = false;
+		  String ans = null;
+		  while (T--> 0) {
+		   String s = br.readLine().trim();
+		   StringTokenizer st = new StringTokenizer(s);
+		   int ch = Integer.parseInt(st.nextToken());
+		   int num = Integer.parseInt(st.nextToken());
+		   if (ch == 1) {
+		    op = MyMath.isOdd();
+		    ret = MyMath.checker(op, num);
+		    ans = (ret) ? "ODD" : "EVEN";
+		   } else if (ch == 2) {
+		    op = MyMath.isPrime();
+		    ret = MyMath.checker(op, num);
+		    ans = (ret) ? "PRIME" : "COMPOSITE";
+		   } else if (ch == 3) {
+		    op = MyMath.isPalindrome();
+		    ret = MyMath.checker(op, num);
+		    ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
+
+		   }
+		   System.out.println(ans);
+		  }
+		 }
+		}
+
+
