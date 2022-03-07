701dbf439a96d3d529ebed1c9218298679012ed0
bhagyashri <yuvabhagyashripatil@gmail.com>
2022-03-08 00:14:17 +0530

    immutable string

---
+++ day12/src/day12/immutableString.java
@@ -0,0 +1,12 @@
+package day12;
+
+public class immutableString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String s="Sachin";  
+		   s=s.concat(" Tendulkar");  
+		   System.out.println(s);  
+	}
+
+}
