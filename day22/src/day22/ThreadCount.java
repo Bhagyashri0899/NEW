2064c295b56f5b0e8e083e7eb977744f973bb394
bhagyashri <yuvabhagyashripatil@gmail.com>
2022-03-08 21:51:04 +0530

    First Program

---
+++ day22/src/day22/ThreadCount.java
@@ -0,0 +1,26 @@
+package day22;
+
+public class ThreadCount extends Thread {
+	
+	 public void run() {
+	      for (int i = 0; i < 7; i++) {
+	         System.out.println( Thread.currentThread().getName());
+	    	 
+	      }
+	   }
+	 
+	public static void main(String[] args) {
+		
+		ThreadCount t1 = new ThreadCount();
+		Thread thread1 = new Thread(t1);
+        Thread thread2 = new Thread(t1);
+
+        thread1.start();
+        thread2.start();  
+	        
+	    	  
+	      }
+
+	}
+
+
