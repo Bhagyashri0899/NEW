06b722bfc36e1b33fbb8cae2cbcc76ea2fac5b2a
bhagyashri <yuvabhagyashripatil@gmail.com>
2022-03-15 21:47:10 +0530

    Hibernate

---
+++ Project_1/src/com/hibernate/App.java
@@ -0,0 +1,45 @@
+package com.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.project_1.Users;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Users.class)
+                .buildSessionFactory();
+
+Session session = factory.getCurrentSession();
+try {
+// Create object of entity class type
+	Users user = new Users();
+//Users user = new Users( 3, "Bhagyashri","Patil","bhagya@gmail.com");
+
+// Start transaction
+session.beginTransaction();
+// Perform operation
+//session.save(user);
+user = session.get(Users.class,5);
+
+//Update object
+user.setUsers_id(2);
+
+// Commit the transaction 
+session.getTransaction().commit();
+System.out.println(user);
+
+
+} finally {
+session.close();
+factory.close();
+}
+
+	}
+
+}
---
+++ Project_1/src/com/hibernate/project_1/Users.java
@@ -0,0 +1,76 @@
+package com.hibernate.project_1;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+
+public class Users {
+	
+	public Users() {
+		
+	}
+
+	public Users(int users_id, String users_fname, String users_lname, String users_email) {
+		this.users_id = users_id;
+		this.users_fname = users_fname;
+		this.users_lname = users_lname;
+		this.users_email = users_email;
+	}
+
+	@Id 
+	@Column(name="users_id")
+	int users_id;
+	
+	@Column(name="users_fname")
+	String users_fname;
+	
+	@Column(name="users_lname")
+	String users_lname;
+	
+	@Column(name="users_email")
+	String users_email;
+
+	public int getUsers_id() {
+		return users_id;
+	}
+
+	public void setUsers_id(int users_id) {
+		this.users_id = users_id;
+	}
+
+	public String getUsers_fname() {
+		return users_fname;
+	}
+
+	public void setUsers_fname(String users_fname) {
+		this.users_fname = users_fname;
+	}
+
+	public String getUsers_lname() {
+		return users_lname;
+	}
+
+	public void setUsers_lname(String users_lname) {
+		this.users_lname = users_lname;
+	}
+
+	public String getUsers_email() {
+		return users_email;
+	}
+
+	public void setUsers_email(String users_email) {
+		this.users_email = users_email;
+	}
+
+	@Override
+	public String toString() {
+		return "Users [users_id=" + users_id + ", users_fname=" + users_fname + ", users_lname=" + users_lname
+				+ ", users_email=" + users_email + "]";
+	}
+	
+}
