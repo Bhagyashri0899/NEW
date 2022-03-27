bdde5c5e36d675bf8694be5140cc7f658be98959
bhagyashri <yuvabhagyashripatil@gmail.com>
2022-03-27 23:31:27 +0530

    one to one relationship

---
+++ JPAOneToOnerelation/associationDemo1/com/cg/jpa/association/Department.java
@@ -0,0 +1,30 @@
+package com.cg.jpa.association;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Department {
+	 @Id 
+	   @GeneratedValue( strategy=GenerationType.AUTO )
+	   private int id;
+	   private String name;
+
+	   public int getId() {
+	      return id;
+	   }
+
+	   public void setId(int id) {
+	      this.id = id;
+	   }
+
+	   public String getName( ) {
+	      return name;
+	   }
+
+	   public void setName( String deptName ) {
+	      this.name = deptName;
+	   }
+}
---
+++ JPAOneToOnerelation/associationDemo1/com/cg/jpa/association/Main.java
@@ -0,0 +1,36 @@
+package com.cg.jpa.association;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+	 public static void main(String[] args) {
+		   
+		   EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "JPA" );
+		   EntityManager entitymanager = emfactory.createEntityManager( );
+		   entitymanager.getTransaction( ).begin( );
+
+		   //Create Department Entity
+		   Department department = new Department();
+		   department.setName("Development");
+
+		   //Store Department
+		   entitymanager.persist(department);
+
+		   //Create Employee Entity
+		   Employee employee = new Employee();
+		   employee.setEname("Satish");
+		   employee.setSalary(45000.0);
+		   employee.setDeg("Technical Writer");
+		   employee.setDepartment(department);
+
+		   //Store Employee
+		   entitymanager.persist(employee);
+
+		   entitymanager.getTransaction().commit();
+		   entitymanager.close();
+		   emfactory.close();
+		   }
+		
+}
