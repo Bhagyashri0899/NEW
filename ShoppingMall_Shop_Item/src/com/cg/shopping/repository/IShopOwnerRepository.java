09f06e404d3fbffc0420b95d30ed1695688868c3
bhagyashri <yuvabhagyashripatil@gmail.com>
2022-04-03 16:15:36 +0530

    project

--- ShoppingMall_Shop_Item/src/com/cg/shopping/repository/IShopOwnerRepository.java
+++ ShoppingMall_Shop_Item/src/com/cg/shopping/repository/IShopOwnerRepository.java
@@ -1,5 +1,13 @@
 package com.cg.shopping.repository;
 
-public class IShopOwnerRepository {
+import com.cg.shopping.entities.ShopOwner;
 
+public interface IShopOwnerRepository 
+{
+    public ShopOwner addShopOwner(ShopOwner shopOwner);
+    public ShopOwner updateShopOwner(ShopOwner shopOwner);
+    
+    public ShopOwner searchShopOwner(int id);     //long id;
+    public ShopOwner deleteShopOwner(int id);    //long id;
+    
 }
--- ShoppingMall_Shop_Item/src/com/cg/shopping/repository/ShopOwnerImpl.java
+++ ShoppingMall_Shop_Item/src/com/cg/shopping/repository/ShopOwnerImpl.java
@@ -1,5 +1,46 @@
 package com.cg.shopping.repository;
 
-public class ShopOwnerImpl {
+import javax.persistence.EntityManager;
+
+import com.cg.shopping.entities.ShopOwner;
+
+public class ShopOwnerImpl implements IShopOwnerRepository
+{
+
+	//Step 1: Start JPA LifeCycle
+	
+    //create entity manager object, and import
+		private EntityManager entityManager;
+		public ShopOwnerImpl() 
+		{
+			entityManager = JPAUtil.getEntityManager();
+		}
+		
+	//Create operation in repository/DAO
+		
+	@Override
+	public ShopOwner addShopOwner(ShopOwner shopOwner) {
+		entityManager.persist(shopOwner);
+		return shopOwner;	
+	}
+
+	@Override
+	public ShopOwner updateShopOwner(ShopOwner shopOwner) {
+		entityManager.merge(shopOwner);
+		return shopOwner ;
+	}
+
+	@Override
+	public ShopOwner searchShopOwner(int id) {
+		ShopOwner shopOwner = entityManager.find(ShopOwner.class,id);
+		return shopOwner;
+	}
+
+	@Override
+	public ShopOwner deleteShopOwner(int id) {
+		ShopOwner shopOwner = entityManager.find(ShopOwner.class,id);
+		entityManager.remove(shopOwner);
+		return shopOwner;
+	}
 
 }
